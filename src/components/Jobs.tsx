cd79010bfc47b0cd36c8e47f0449e3f023dc6f95
MohamedAli09 <mohamed.ali00870@gmail.com>
2025-02-02 22:34:06 +0200

    refactor: extract jobFields data to separate file in Jobs component

--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
 import jobs from "../data/jobs";
+
 import {
   Search,
   Briefcase,
@@ -10,44 +11,13 @@ import {
   Filter,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import jobFields from "../data/jobFields";
 
 const Jobs: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedField, setSelectedField] = useState("");
   const navigate = useNavigate();
 
-  const jobFields = [
-    {
-      name: "تكنولوجيا المعلومات",
-      image:
-        "https://www.rasmussen.edu/images/rasmussenlibraries/blogs/work-from-home-tech-jobs-s.jpg", // Technology/IT image
-    },
-    {
-      name: "الهندسة",
-      image:
-        "https://storables.com/wp-content/uploads/2023/11/why-is-it-called-civil-engineering-1699953005.jpg", // Engineering image
-    },
-    {
-      name: "الطب والرعاية الصحية",
-      image:
-        "https://cdn.prod.website-files.com/6466101d017ab9d60c8d0137/65df25f0a339915ec6c00de7_Out%20of%20Hospital%20Costs_Savings%20for%20Medical%20Schemes.jpg", // Healthcare image
-    },
-    {
-      name: "التعليم",
-      image:
-        "https://education.illinoisstate.edu/images/academics/teaching-learning/teacher-learning-hero.jpg", // Education image
-    },
-    {
-      name: "الأعمال والإدارة",
-      image:
-        "https://cloudinary.hbs.edu/hbsit/image/upload/s--GpBHn1g1--/f_auto,c_fill,h_375,w_750,/v20200101/DDB997885D475494DB0A0440F658772C.jpg", // Business image
-    },
-    {
-      name: "العلوم",
-      image:
-        "https://www.lawsonstate.edu/_resources/assets/img/sciencebanner.jpg", // Science image
-    },
-  ];
   const handleJobClick = (job: { id: string | number }) => {
     navigate(`/jobs/${job.id}`, { state: { job } });
   };
